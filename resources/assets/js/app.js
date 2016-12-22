4345bb181041c18264e4f076d76afae530fa4308
FAOZI <faozimipa@gmail.com>
2016-12-22 23:47:11 +0700

    setting app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,9 +12,43 @@ require('./bootstrap');
  * the body of the page. From here, you may begin adding components to
  * the application, or feel free to tweak this setup for your needs.
  */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.component('example', require('./components/Example.vue'));
+import Example from './components/Example.vue'
 
-const app = new Vue({
-    el: '#app'
-});
+//Vue.component('example', require('./components/Example.vue'));
+
+//use Plugin
+Vue.use(VueRouter)
+
+
+// 2. Define route components
+const Home = { template: '<div>home</div>' }
+const Foo = { template: '<div>Faozi</div>' }
+const Bar = { template: '<div>Bar</div>' }
+
+// 3. Create the router
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+    { path: '/example', component: Example },
+    { path: '/rooms', component: Room } 
+   ]
+})
+
+// 4. Create and mount root instance.
+// Make sure to inject the router.
+// Route components will be rendered inside <router-view>.
+new Vue({
+  router,
+  components : {
+    Example,
+  },
+  data : {
+  } 
+}).$mount('#app')
